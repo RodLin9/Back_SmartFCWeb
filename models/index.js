dd93ec45570b05069a50c664716bc1ec4db01923
LinaR <linaferrodri@unicauca.edu.co>
2023-10-22 22:29:15 -0500

    Eliminación archivos sin uso

--- models/index.js
+++
@@ -1,9 +0,0 @@
-//const { modelNames } = require("mongoose")
-
-const models = {
-    dudasModel: require('./nosql/dudas_model'),
-    estudianteModel: require('./nosql/estudiante_model'),
-    schoolModel: require('./nosql/school_model')
-}
-
-module.exports = models
